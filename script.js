ee03ba0046525f70584a71a2b40838e4fbc38cb9
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-01-31 12:15:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-const arr = [0, 1, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+// const arr = [0, 1, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
 // https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
 
 //0=>0 1=>1 2=>abc 3=>def 4=>ghi 5=>jkl 6=>mno 7=>pqrs 8=>tuv 9=>wxyz
 
-function letterCombinations(input_digit) {
+function letterCombinations(digits) {
   //Complete the function
-  let ans = [];
-  let lastDigit = input_digit%10;
-  let firstDigit = Math.floor(input_digit/10);
-  let str1 = String(arr[firstDigit]);
-  // console.log(`str1: ${str1}`);
-  let str2 = String(arr[lastDigit]);
-  // console.log(`str2: ${str2}`);
+  if (digits == null || digits.length === 0) return [];
 
-  for (let i = 0; i < str1.length; i++) {
-    let x = String(str1[i]);
-    for(let j = 0; j < str2.length; j++){
-      ans.push(`${x}${String(str2[j])}`);
+  const map = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz',
+  };
+
+  const res = [];
+  const go = (i, s) => {
+    if (i === digits.length) {
+      res.push(s);
+      return;
     }
-    
-  }
-  
-  console.log(ans.sort());
-  // return ans.sort();
+
+    for (const c of map[digits[i]]) {
+      go(i + 1, s + c);
+    }
+  };
+
+  go(0, '');
+  return res;
 }
 
-letterCombinations(23);
+//letterCombinations(23);
        
 module.exports = letterCombinations;
 
